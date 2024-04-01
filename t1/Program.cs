f489d5bd7139c7b28d0481c8c5c9b765bfdbbafe
unknown <idenisov@nt-rst-b116.rst.lan>
2024-04-01 13:08:50 +0300

    the git is initialized

---
+++ t1/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program 
+{
+static void Main(string[] args)
+
+{
+    int M = 2;
+    int N = 10;
+    PrintNumbersRecursive2(M,N);
+}
+
+private static void PrintNumbersRecursive2 (int m,int n)
+
+{ 
+    if (m==n)
+    Console.WriteLine(m);
+    else
+    {
+    PrintNumbersRecursive2(m+1,n);
+    Console.WriteLine(m);  
+    }
+}
+}
