5f5bea577aea6168d7b0a26af430550276568c75
unknown <idenisov@nt-rst-b116.rst.lan>
2024-04-01 13:52:04 +0300

    mission complete

---
+++ t2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace RecursiveArrayTraversal
+{
+class Program
+{
+static void Main(string[] args)
+{
+int[] arr = new int[] { 1, 2, 3, 4, 5 };
+RecursivePrintElements(arr, arr.Length - 1);
+}
+    static void RecursivePrintElements(int[] arr, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.Write(arr[index] + " ");
+        RecursivePrintElements(arr, index - 1);
+    }
+}
+}
---
+++ t3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AkkermanFun
+{
+class Program
+{
+
+    public static int CalculateAkkermanFunction(int m, int n)
+    {
+    if (m == 0) return n + 1;
+    if (n == 0) 
+        return CalculateAkkermanFunction(m - 1, 1); 
+    
+    return CalculateAkkermanFunction(m - 1, CalculateAkkermanFunction(m, n - 1));
+    }
+
+    static void Main(string[] args)
+    {
+        System.Console.Write("Введите первое число: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("Введите второе число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(CalculateAkkermanFunction(m, n));
+    }
+}
+}
